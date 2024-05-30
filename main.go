7965b1ecb9a1bf07b0eefaff532dfe0246028555
nemopss <alexey_gladilin@mail.ru>
2024-05-30 22:58:30 +0300

    Added CSV reader

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in form of 'question,answer'")
 	flag.Parse()
-	_ = csvFilename
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	_ = r
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
